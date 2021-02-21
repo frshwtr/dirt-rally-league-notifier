70690a513121d249d9166171672498b04387d361
ben <ben@frshwtr.uk>
2021-02-21 19:10:08 +0000

    feature: using external service to get RaceNet data

--- src/adapters/RaceNet/getLeagueData.ts
+++ src/adapters/RaceNet/getLeagueData.ts
@@ -1,14 +1,19 @@
 import { League } from "../../model/League";
+import * as fetch from "node-fetch";
 
-export const getLeagueData = (): League => ({
-    leagueName: "myLeague",
-    board: [
-        {
-            position: 1,
-            name: "Ben",
-            time: "1234",
-            playerDiff: "1234",
-        },
-        { position: 2, name: "Ben", time: "1234", playerDiff: "1234" },
-    ],
-});
+export const getLeagueData = async (): Promise<League> => {
+    await fetch("https://www.dirtgame.com/dirtrally/uk/api/event");
+
+    return {
+        leagueName: "myLeague",
+        board: [
+            {
+                position: 1,
+                name: "Ben",
+                time: "1234",
+                playerDiff: "1234"
+            },
+            { position: 2, name: "Ben", time: "1234", playerDiff: "1234" }
+        ]
+    };
+};
--- test/getLeagueData.test.ts
+++ test/getLeagueData.test.ts
@@ -1,16 +1,36 @@
 import { League } from "../src/model/League";
 import { getLeagueData } from "../src/adapters/RaceNet/getLeagueData";
+import * as fetch from "node-fetch";
 
-describe("when getting league data from RaceNet", () => {
-    it("should return the league data", () => {
-        const expectedLeagueData: League = {
-            board: [
-                { position: 1, name: "Ben", time: "1234", playerDiff: "1234" },
-                { position: 2, name: "Ben", time: "1234", playerDiff: "1234" },
-            ],
-            leagueName: "myLeague",
-        };
+jest.mock("node-fetch");
+const mockFetch = <jest.Mock<fetch>>fetch;
+describe("when getting league data from RaceNet", async () => {
+    const expectedLeagueData: League = {
+        board: [
+            {
+                position: 1,
+                name: "Ben",
+                time: "1234",
+                playerDiff: "1234"
+            },
+            {
+                position: 2,
+                name: "Ben",
+                time: "1234",
+                playerDiff: "1234"
+            }
+        ],
+        leagueName: "myLeague"
+    };
 
-        expect(getLeagueData()).toBe(expectedLeagueData);
+    mockFetch.mockResolvedValue({ status: 200 });
+
+    it("should return the league data", async () => {
+        expect(await getLeagueData()).toEqual(expectedLeagueData);
+    });
+
+    it("should call an external service", async () => {
+        await getLeagueData();
+        expect(mockFetch).toBeCalled();
     });
 });
